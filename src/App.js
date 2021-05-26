5c5fb02bf6548b1efa3508c3587cb6a00dd59634
hosibito <hosibito@naver.com>
2021-05-26 22:42:08 +0900

    3.3 Planning the Movie Component (05:02)

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import React from 'react';
-
-
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("constructor!!  ( 1 )");
+  state = {
+    isLoading: true,
+    movies: []
   }
 
-  state ={
-    count:0
-  };
-
-  add = () => {
-    //this.state.count = 1;   // 이렇게 변경이 안된다. 또한  render 하지 않아 화면에 반영이 안된다. 
-      // Line 10:5:  Do not mutate state directly. Use setState()  react/no-direct-mutation-state 경고뜬다.
-    // this.setState({ count: this.state.count + 1 })  이렇게 사용하면 좋지 못하다. 
-    this.setState( current => ( { count: current.count + 1 } ) )  // setState 에서 현제 스테더스값을 제공한다. 
-  
-  };
-  minus = () => {
-    //this.setState({ count: this.state.count - 1})
-    this.setState( current => ( { count: current.count - 1 } ) )
-  };
-
-  componentDidMount(){
-    console.log("componentDidMount!! ( 3 )")
+  componentDidMount() {
+    setTimeout(()=>{
+      this.setState({isLoading:false});
+    }, 6000);
   }
 
-  componentDidUpdate(){
-    console.log("componentDidUpdate!! 더하기 뺴기 눌렀을때!! ")
-  }
-
-  componentWillUnmount(){
-    console.log("componentWillUnmount 닫거나 다른창으로 갈때!! ")
-  }
-
-  render(){
-    console.log("render!  ( 2 )")
+  render(){  
+    const { isLoading } = this.state;
     return( 
     <div>
-      <h1>클래스 컴포넌트 스테이트 숫자 : {this.state.count}</h1>
-      <button onClick={this.add}>더하기</button>
-      <button onClick={this.minus}>빼기</button>
+      { isLoading ? "로딩중..." : "준비가 완료되었습니다." }
     </div>
     );
   }
