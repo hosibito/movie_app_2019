102df046815692fac58f72236586024e4f64efde
hosibito <hosibito@naver.com>
2021-05-26 19:06:40 +0900

    2.2 Dynamic Component Generation (09:54)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React from 'react';
 
-function Food({fav}){         //function Food(props)
-  //console.log(fav);     // console.log(props.fav)
-  return <h1>난 {fav}를 좋아한다.</h1>;
+function Food({fav , picture }){         
+  return <div>
+        <h2>난 {fav}를 좋아한다.</h2>
+        <img src={picture} />
+    </div>;
 }
 
+const foodILike = [
+  {
+    name: "김치",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "삼겹살",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "비빔밥",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "돈까스",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "김밥",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div >
        <h1>안녕안녕!!!!</h1>
-       <Food fav="김치" />
-       <Food fav="라면" />
-       <Food fav="삼겹살" />
-       <Food fav="족발" />
+       { foodILike.map( (dish) => <Food fav={dish.name} picture={dish.image}/> ) }
     </div>
   );
 }
