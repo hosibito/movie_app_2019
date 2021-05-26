0a1de0aede6dedb50b5e4bf01ad09a71299c8eec
hosibito <hosibito@naver.com>
2021-05-26 18:52:53 +0900

    2.1 Reusable Components with JSX + Props (09:13)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({fav}){         //function Food(props)
+  //console.log(fav);     // console.log(props.fav)
+  return <h1>난 {fav}를 좋아한다.</h1>;
+}
+
 
 function App() {
   return (
     <div >
        <h1>안녕안녕!!!!</h1>
-       <Potato />
+       <Food fav="김치" />
+       <Food fav="라면" />
+       <Food fav="삼겹살" />
+       <Food fav="족발" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Potato(){
-    return <h3>나는 감자를 사랑한다.</h3>;
-}
-
-export default Potato;
